2296195849f7823c756f348df2cdad0416ad4424
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-10-02 14:56:34 +0900

    GameSceneの追加

---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <KamataEngine.h>
+
+using namespace KamataEngine;
+
+/// <summary>
+///ゲームシーン
+/// </summary>
+class GameScene {
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+};
+
+
